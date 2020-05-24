f832893c207128a98ce2ee9f5cfa82bb5a38a08f
vovanquang12cntt <vanquang451@gmail.com>
2020-05-24 18:40:34 +0700

    [Add] Apply theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,20 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "@fortawesome/fontawesome-free/js/all";
+require("bootstrap");
+require("@fortawesome/fontawesome-free");
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require('jquery')
+require("packs/aos")
+require("packs/bootstrap-datepicker.min")
+require("packs/mediaelement-and-player.min")
+require("packs/popper.min")
+require("packs/slick.min")
+require("packs/typed")
+require("packs/main")
